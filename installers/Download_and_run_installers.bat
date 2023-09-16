c65791e65d9ddc76212c7a78c5048a2c1b68267a
ThomasWarn <thomaswarnasch@gmail.com>
2023-09-15 19:58:42 -0500

    Create Download_and_run_installers.bat

---
+++ installers/Download_and_run_installers.bat
@@ -0,0 +1,8 @@
+curl --location --remote-header-name --remote-name https://raw.githubusercontent.com/ThomasWarn/bug-free-octo-eureka/main/installers/01_install_vs2019_tools.bat
+curl --location --remote-header-name --remote-name https://raw.githubusercontent.com/ThomasWarn/bug-free-octo-eureka/main/installers/02_install_anaconda.bat
+curl --location --remote-header-name --remote-name https://raw.githubusercontent.com/ThomasWarn/bug-free-octo-eureka/main/installers/03_install_cuda.bat
+curl --location --remote-header-name --remote-name https://raw.githubusercontent.com/ThomasWarn/bug-free-octo-eureka/main/installers/04_install_git.bat
+.\01_install_vs2019_tools.bat
+.\02_install_anaconda.bat
+.\03_install_cuda.bat
+.\04_install_git.bat
